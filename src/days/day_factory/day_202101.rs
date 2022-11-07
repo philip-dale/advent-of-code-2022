55d6a6f25d84ffea515fa0cf4b7a576af436fd80
Phil Dale <dalephilip@gmail.com>
2022-11-07 21:37:19 +0000

    implemented 202101 as test of system

--- src/days/day_factory/day_202101.rs
+++ src/days/day_factory/day_202101.rs
@@ -7,12 +7,30 @@ pub struct Day202101{}
 impl Day for Day202101 {
     fn run1(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: Vec<u64> = ipr.vec_1d()?;
-        return Ok(data.len().to_string());
+        let mut last = data[0];
+        let mut count = 0;
+        for p in 1..data.len() {
+            if data[p] > last {
+                count += 1;
+            }
+            last = data[p];
+        }
+
+        return Ok(count.to_string());
     }
     
     fn run2(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: Vec<u64> = ipr.vec_1d()?;
-        return Ok(data.len().to_string());
+        let mut last = data[0] + data[1] + data[2];
+        let mut count = 0;
+        for p in 3..data.len()-2 {
+            let val = data[p] + data[p+1] + data[p+2];
+            if val > last {
+                count += 1;
+            }
+            last = val;
+        }
+        return Ok(count.to_string());
     }
 }
 
