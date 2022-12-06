9f8a7f94f108302f34cd26de2cc7d59e825a62c8
philip-dale <78593492+philip-dale@users.noreply.github.com>
2022-12-06 06:25:05 +0000

    first pass of day 6 2022

--- src/days/day_factory/year_2022/day_06.rs
+++ src/days/day_factory/year_2022/day_06.rs
@@ -4,12 +4,39 @@ use crate::days::day_factory::Day;
 
 pub struct Day06{}
 
+fn check_n(ch: &[char], n:usize) -> bool {
+    for i in 1..n {
+        if ch[0] == ch[i] {
+            return false;
+        }
+    }
+
+    if n > 2 {
+        return check_n(&ch[1..], n-1);
+    }
+    true
+}
+ 
+fn get_start(s: &String, n:usize) -> usize{
+    let ch: Vec<char> = s.chars().collect();
+    for i in 0..ch.len()-n {
+        if check_n(&ch[i..], n) {
+            return i+n;
+        }
+    }
+
+    0
+}
+
 impl Day for Day06 {
     fn run1(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
-        Ok(ipr.fullname()?)
+        let data: String = ipr.whole()?;
+
+        Ok(get_start(&data, 4).to_string())
     }
     
     fn run2(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
-        Ok(ipr.fullname()?)
+        let data: String = ipr.whole()?;
+        Ok(get_start(&data,14).to_string())
     }
 }
