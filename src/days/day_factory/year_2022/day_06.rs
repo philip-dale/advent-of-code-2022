5f6b89830b345bf8a31b6626feee715936e101ba
philip-dale <78593492+philip-dale@users.noreply.github.com>
2022-12-06 07:22:28 +0000

    using optional

--- src/days/day_factory/year_2022/day_06.rs
+++ src/days/day_factory/year_2022/day_06.rs
@@ -17,26 +17,30 @@ fn check_n(ch: &[char], n:usize) -> bool {
     true
 }
  
-fn get_start(s: &str, n:usize) -> usize{
+fn get_start(s: &str, n:usize) -> Option<usize>{
     let ch: Vec<char> = s.chars().collect();
     for i in 0..ch.len()-n {
         if check_n(&ch[i..], n) {
-            return i+n;
+            return Some(i+n);
         }
     }
-
-    0
+    None
 }
 
 impl Day for Day06 {
     fn run1(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: String = ipr.whole()?;
-
-        Ok(get_start(&data, 4).to_string())
+        match get_start(&data, 4) {
+            Some(n) => Ok(n.to_string()),
+            None => Ok(String::from("Not found")),
+        }
     }
     
     fn run2(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: String = ipr.whole()?;
-        Ok(get_start(&data,14).to_string())
+        match get_start(&data,14) {
+            Some(n) => Ok(n.to_string()),
+            None => Ok(String::from("Not found")),
+        }
     }
 }
