00a4089960576a1512dece8d90653e2c7e4e4620
philip-dale <78593492+philip-dale@users.noreply.github.com>
2022-12-08 17:10:03 +0000

    better loops

--- src/days/day_factory/year_2022/day_08.rs
+++ src/days/day_factory/year_2022/day_08.rs
@@ -3,12 +3,14 @@ use crate::input_reader;
 use crate::days::day_factory::Day;
 use crate::days::day_factory::types::{CharNumGrid, Point};
 
+static DELTAS: &[(i32, i32)] = &[(-1, 0), (1, 0), (0, -1), (0, 1)];
+
 impl CharNumGrid  {
     pub fn check_points(&self) -> u64{
         let mut total = 0;
         for x in 0..self.cells.len() {
             for y in 0..self.cells[0].len() {
-                if !self.check_point(Point{x,y}) {
+                if self.check_point(Point{x,y}) {
                     total += 1;
                 }
             }
@@ -17,39 +19,27 @@ impl CharNumGrid  {
     }
 
     pub fn check_point(&self, p: Point) -> bool {
-        let x_max_value = self.cells.len();
-        let y_max_value = self.cells[0].len();
+        let x_max_value = self.cells.len() as i32;
+        let y_max_value = self.cells[0].len() as i32;
 
-        let mut hidden1 = false;
-        for x in p.x+1..x_max_value {
-            if self.cells[x][p.y] >= self.cells[p.x][p.y] {
-                hidden1 = true;
+        for (dx, dy) in DELTAS {
+            let mut x = p.x as i32;
+            let mut y = p.y as i32;
+            let mut loop_vis = true;
+            loop {
+                x += dx;
+                y += dy;
+                if x < 0 || x >= x_max_value || y < 0 || y >= y_max_value {
+                    break;
+                }
+                if self.cells[x as usize][y as usize] >= self.cells[p.x][p.y] {
+                    loop_vis = false;
+                    break;
+                }
             }
-        }
-
-        let mut hidden2 = false;
-        for x in (0..p.x).rev() {
-            if self.cells[x][p.y] >= self.cells[p.x][p.y] {
-                hidden2 = true;
+            if loop_vis {
+                return true;
             }
-        }
-
-        let mut hidden3 = false;
-        for y in p.y+1..y_max_value {
-            if self.cells[p.x][y] >= self.cells[p.x][p.y] {
-                hidden3 = true;
-            }
-        }
-
-        let mut hidden4 = false;
-        for y in (0..p.y).rev() {
-            if self.cells[p.x][y] >= self.cells[p.x][p.y] {
-                hidden4 = true;
-            }
-        }
-
-        if hidden1 && hidden2 && hidden3 && hidden4 {
-            return true;
         }
         false
     }
@@ -68,42 +58,28 @@ impl CharNumGrid  {
     }
 
     pub fn check_view(&self, p: Point) -> u32 {
-        let x_max_value = self.cells.len();
-        let y_max_value = self.cells[0].len();
+        let mut count = 1; // using multiples
+        let x_max_value = self.cells.len() as i32;
+        let y_max_value = self.cells[0].len() as i32;
 
-        let mut dist1 = 0;
-        for x in p.x+1..x_max_value {
-            dist1 += 1;
-            if self.cells[x][p.y] >= self.cells[p.x][p.y] {
-                break;
+        for (dx, dy) in DELTAS {
+            let mut x = p.x as i32;
+            let mut y = p.y as i32;
+            let mut loop_count = 0;
+            loop {
+                x += dx;
+                y += dy;
+                if x < 0 || x >= x_max_value || y < 0 || y >= y_max_value {
+                    break;
+                }
+                loop_count += 1;
+                if self.cells[x as usize][y as usize] >= self.cells[p.x][p.y] {
+                    break;
+                }
             }
+            count *= loop_count;
         }
-
-        let mut dist2 = 0;
-        for x in (0..p.x).rev() {
-            dist2 += 1;
-            if self.cells[x][p.y] >= self.cells[p.x][p.y] {
-                break;
-            }
-        }
-
-        let mut dist3 = 0;
-        for y in p.y+1..y_max_value {
-            dist3 += 1;
-            if self.cells[p.x][y] >= self.cells[p.x][p.y] {
-                break;
-            }
-        }
-
-        let mut dist4 = 0;
-        for y in (0..p.y).rev() {
-            dist4 += 1;
-            if self.cells[p.x][y] >= self.cells[p.x][p.y] {
-                break;
-            }
-        }
-
-        dist1 * dist2 * dist3 *dist4
+        count
     }
 
 }
