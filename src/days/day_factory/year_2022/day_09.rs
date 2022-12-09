400177f0d1eebeac8fbe12b7d6028849a797dadd
philip-dale <78593492+philip-dale@users.noreply.github.com>
2022-12-09 21:56:45 +0000

    clean u

--- src/days/day_factory/year_2022/day_09.rs
+++ src/days/day_factory/year_2022/day_09.rs
@@ -41,9 +41,7 @@ impl Rope {
             for k in 1..self.knots.len(){
                 self.move_tail(k);
             }
-            
             self.visited.entry(*self.knots.last().unwrap()).and_modify(|c| *c += 1).or_insert(0);
-            //println!("****");
         }
     }
 
@@ -52,41 +50,26 @@ impl Rope {
         let y_delta = self.knots[i-1].1 - self.knots[i].1;
 
         if x_delta.abs() > 1 || y_delta.abs() > 1 {
-            //println!("{0}: {1},{2} - {3},{4} : dx = {5}, dy = {6}", i, self.knots[i-1].0, self.knots[i-1].1, self.knots[i].0, self.knots[i].1, x_delta, y_delta);
-            if x_delta.abs() == 2 && y_delta.abs() == 2{
+            if x_delta.abs() == 2 {
                 if x_delta > 0 {
                     self.knots[i].0 = self.knots[i-1].0-1;
                 } else {
                     self.knots[i].0 = self.knots[i-1].0+1;
                 }
-
-                if y_delta > 0 {
-                    self.knots[i].1 = self.knots[i-1].1-1;
-                } else {
-                    self.knots[i].1 = self.knots[i-1].1+1;
-                }
-                //self.knots[i].0 += x_delta/2;
-                //self.knots[i].1 += y_delta/2;
-            } else if x_delta.abs() > y_delta.abs() {
-                //self.knots[i].0 += x_delta;
-                if x_delta > 0 {
-                    self.knots[i].0 = self.knots[i-1].0-1;
-                } else {
-                    self.knots[i].0 = self.knots[i-1].0+1;
-                }
-                self.knots[i].1 = self.knots[i-1].1;
-            } else {
-                //self.knots[i].1 += y_delta;
-                if y_delta > 0 {
-                    self.knots[i].1 = self.knots[i-1].1-1;
-                } else {
-                    self.knots[i].1 = self.knots[i-1].1+1;
-                }
+            } else  {
                 self.knots[i].0 = self.knots[i-1].0;
             }
-            //println!("{0},{1} : {2},{3}", self.knots[i-1].0, self.knots[i-1].1, self.knots[i].0, self.knots[i].1);
+
+            if y_delta.abs() == 2{
+                if y_delta > 0 {
+                    self.knots[i].1 = self.knots[i-1].1-1;
+                } else {
+                    self.knots[i].1 = self.knots[i-1].1+1;
+                }
+            } else {
+                self.knots[i].1 = self.knots[i-1].1;
+            }
         }
-        //println!("h = {0},{1} - t = {2},{3}", self.knots[0].0, self.knots[0].1, self.knots[1].0, self.knots[1].1);
     }
 }
 
@@ -95,31 +78,19 @@ pub struct Day09{}
 impl Day for Day09 {
     fn run1(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: Vec<Instruction> = ipr.vec_1d_newln()?;
-
         let mut rope = Rope::new(2);
-
         for i in &data {
             rope.apply_instruction(i);
         }
-
         Ok(rope.visited.len().to_string())
     }
     
     fn run2(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: Vec<Instruction> = ipr.vec_1d_newln()?;
-
         let mut rope = Rope::new(10);
-
         for i in &data {
             rope.apply_instruction(i);
-            //for k in &rope.knots {
-                //println!("{0},{1}", k.0, k.1);
-            //}
-            //println!("___");
         }
-        //for k in rope.visited.keys() {
-        //    println!("{0},{1}",k.0, k.1);
-        //}
         Ok(rope.visited.len().to_string())
     }
 }
