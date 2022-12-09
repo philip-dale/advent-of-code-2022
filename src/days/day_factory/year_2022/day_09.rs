88f40826bcee0e3efc76f787e58c70b0459721a8
Phil Dale <dalephilip@gmail.com>
2022-12-09 12:35:51 +0000

    2022 day 9 part 2 not working

--- src/days/day_factory/year_2022/day_09.rs
+++ src/days/day_factory/year_2022/day_09.rs
@@ -19,48 +19,57 @@ impl std::str::FromStr for Instruction {
 }
 
 struct Rope {
-    h: (i32, i32),
-    t: (i32, i32),
+    knots: Vec<(i32, i32)>,
     visited: HashMap<(i32, i32), u32>,
 }
 
 impl Rope {
+    pub fn new(size: usize) -> Self {
+        Self{
+            knots: vec![(0,0); size],
+            visited: HashMap::from([((0,0),0)]),
+        }
+    }
+
     pub fn apply_instruction(& mut self, instruction: &Instruction) {
         let direction_move: HashMap<char, (i32, i32)> = HashMap::from([('R', (1,0)),('L', (-1,0)),('U', (0,1)),('D', (0,-1))]);
         let delta = direction_move.get(&instruction.direction).unwrap();
         for _c in 0..instruction.distance {
-            self.h.0 += delta.0;
-            self.h.1 += delta.1;
+            self.knots[0].0 += delta.0;
+            self.knots[0].1 += delta.1;
 
-            self.move_tail();
+            for k in 1..self.knots.len(){
+                self.move_tail(k);
+            }
+            
+            self.visited.entry(*self.knots.last().unwrap()).and_modify(|c| *c += 1).or_insert(0);
         }
     }
 
-    fn move_tail(& mut self) {
-        let x_delta = self.h.0 - self.t.0;
-        let y_delta = self.h.1 - self.t.1;
+    fn move_tail(& mut self, i: usize) {
+        let x_delta = self.knots[i-1].0 - self.knots[i].0;
+        let y_delta = self.knots[i-1].1 - self.knots[i].1;
 
         if x_delta.abs() > 1 || y_delta.abs() > 1 {
             if x_delta.abs() > y_delta.abs() {
-                self.t.0 += x_delta;
+                self.knots[i].0 += x_delta;
                 if x_delta > 0 {
-                    self.t.0 -= 1;
+                    self.knots[i].0 -= 1;
                 } else {
-                    self.t.0 += 1;
+                    self.knots[i].0 += 1;
                 }
-                self.t.1 = self.h.1;
+                self.knots[i].1 = self.knots[i-1].1;
             } else {
-                self.t.1 += y_delta;
+                self.knots[i].1 += y_delta;
                 if y_delta > 0 {
-                    self.t.1 -= 1;
+                    self.knots[i].1 -= 1;
                 } else {
-                    self.t.1 += 1;
+                    self.knots[i].1 += 1;
                 }
-                self.t.0 = self.h.0;
+                self.knots[i].0 = self.knots[i-1].0;
             }
-            self.visited.entry(self.t).and_modify(|c| *c += 1).or_insert(0);
         }
-        // println!("h = {0},{1} - t = {2},{3}", self.h.0, self.h.1, self.t.0, self.t.1);
+        // println!("h = {0},{1} - t = {2},{3}", self.knots[0].0, self.knots[0].1, self.knots[1].0, self.knots[1].1);
     }
 }
 
@@ -70,22 +79,26 @@ impl Day for Day09 {
     fn run1(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data: Vec<Instruction> = ipr.vec_1d_newln()?;
 
-        let mut rope = Rope{
-            h: (0,0),
-            t: (0,0),
-            visited: HashMap::new(),
-        };
+        let mut rope = Rope::new(2);
         rope.visited.insert((0,0), 0);
 
         for i in data {
             rope.apply_instruction(&i);
-            
         }
 
         Ok(rope.visited.len().to_string())
     }
     
     fn run2(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
-        Ok(ipr.fullname()?)
+        let data: Vec<Instruction> = ipr.vec_1d_newln()?;
+
+        let mut rope = Rope::new(10);
+        rope.visited.insert((0,0), 0);
+
+        for i in data {
+            rope.apply_instruction(&i);
+        }
+
+        Ok(rope.visited.len().to_string())
     }
 }
