3a6fd4f41fd30e63e627ab73804aa86ba9d546fc
philip-dale <78593492+philip-dale@users.noreply.github.com>
2022-12-15 13:21:51 +0000

    working part 2 20222 day 15

--- src/days/day_factory/year_2022/day_15.rs
+++ src/days/day_factory/year_2022/day_15.rs
@@ -9,6 +9,9 @@ const TARGET_LINE:i64 = 2000000;
 // const TARGET_LINE:i64 = 10;
 
 const TARGET_AREA:i64 = 4000000;
+// const TARGET_AREA:i64 = 20;
+
+static DELTAS: &[(i64, i64)] = &[(-1, 1), (1, 1), (1, -1), (-1, -1)];
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone)]
 pub struct SPoint {
@@ -36,9 +39,8 @@ impl std::str::FromStr for SPoint {
 }
 
 struct PointSet {
-    s: HashSet<SPoint>,
     beacons: HashSet<SPoint>,
-    sensors: HashSet<SPoint>,
+    sensors: HashSet<(SPoint, i64)>,
     x_min: i64,
     x_max: i64,
     y_min: i64,
@@ -46,66 +48,54 @@ struct PointSet {
 }
 
 impl PointSet {
-    pub fn add_sensor(& mut self, s:SPoint, d:i64) {
-        let line_delta = (TARGET_LINE - s.y).abs();
-        for x in 0..d-line_delta+1 {
-            let x_pos = s.x+x;
-            let y_pos = TARGET_LINE;
-            let x_neg = s.x-x;
-            self.add_point(SPoint{x: x_pos,y: y_pos});
-            self.add_point(SPoint{x: x_neg,y: y_pos});
-        }
-    }
-
-    pub fn add_point(& mut self, s:SPoint) {
-        
-        if s.x < self.x_min {
-            self.x_min = s.x;
-        }
-        if s.x > self.x_max {
-            self.x_max = s.x;
-        }
-        if s.y < self.y_min {
-            self.y_min = s.y;
-        }
-        if s.y > self.y_max {
-            self.y_max = s.y;
-        }
-
-        self.s.insert(s);
+    pub fn in_range(x:i64, y:i64, sensor:&(SPoint, i64)) -> bool {
+        let d = sensor.0.mhd(&SPoint{x,y});
+        d <= sensor.1
     }
 
     pub fn count_y_line(&self, y:i64) -> u64{
         let mut total = 0;
-        for x in self.x_min..self.x_max+1 {
-            if self.s.contains(&SPoint{x,y}) {
+        'outer: for x in self.x_min..self.x_max+1 {
+            for s in &self.sensors {
                 if self.beacons.contains(&SPoint{x,y}) {
-                    continue;
+                    continue 'outer;
                 }
-                total += 1;
+                if Self::in_range(x, y, s) {
+                    total += 1;
+                    continue 'outer;
+                }
             }
         }
         total
     }
 
-    pub fn print(&self) {
-        for y in self.y_min..self.y_max+1 {
-            print!("{} ", y);
-            for x in self.x_min..self.x_max+1 {
-                if self.s.contains(&SPoint{x,y}) {
-                    if self.beacons.contains(&SPoint{x,y}) {
-                        print!("B", );
-                    } else {
-                        print!("#", );
+    pub fn empty_spot(&self, a:i64) -> i64{
+        // looking for a point that is d+1 away from all shapes and in our target area
+        for (p, r) in &self.sensors {
+
+            for dx in 0..r+2{
+                let dy = r-dx+1;
+                
+                for (sx, sy) in DELTAS {
+                    let x = p.x+(dx*sx);
+                    let y = p.y+(dy*sy);
+                    if !((0..=a).contains(&x) && (0..=a).contains(&y)){
+                        continue
                     }
-                    
-                } else {
-                    print!(".", );
+                    let mut inside = false;
+                    for s in &self.sensors {
+                        if Self::in_range(x, y, s) {
+                            inside = true;
+                        }
+                    }
+                    if !inside {
+                        println!("{}, {}",x,y);
+                        return (x * 4000000) + y;
+                    }
                 }
-                    
             }
-            println!();
         }
+        0
     }
 }
 
@@ -115,7 +105,6 @@ impl std::str::FromStr for PointSet {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let ls: Vec<&str> = s.lines().collect();
         let mut ps = Self{
-            s: HashSet::new(),
             beacons: HashSet::new(),
             sensors: HashSet::new(),
             x_min: i64::MAX,
@@ -126,48 +115,29 @@ impl std::str::FromStr for PointSet {
         for l in ls {
             let re = Regex::new(r"Sensor at (x=-?\d+, y=-?\d+): closest beacon is at (x=-?\d+, y=-?\d+)").unwrap();
             let caps = re.captures(l).unwrap();
-            // println!("{} - {}", &caps[1], &caps[2]);
             let sensor: SPoint = caps[1].parse()?;
             let beacon: SPoint = caps[2].parse()?;
-            ps.beacons.insert(beacon.clone());
-            ps.sensors.insert(sensor.clone());
+
             let d = sensor.mhd(&beacon);
-
+            ps.beacons.insert(beacon.clone());
+            ps.sensors.insert((sensor.clone(),d));
             
-            // let x_range = sensor.x-d..sensor.x+d;
-            println!("{} - {} : {}", &caps[1], &caps[2], d);
-
-            // let max_y = d - (sensor.y - TARGET_LINE).abs();
-            // if max_y < 0 {
-            //     continue;
-            // }
-            // ps.add_sensor(sensor.clone(), d);
-
-
             let y_range = sensor.y-d..sensor.y+d;
             let x_range = sensor.x-d..sensor.x+d;
 
-            if (y_range.start < TARGET_AREA && y_range.start > 0) || (y_range.end < TARGET_AREA && y_range.end > 0) && 
-               (x_range.start < TARGET_AREA && x_range.start > 0) || (x_range.end < TARGET_AREA && x_range.end > 0) {
-                
-                for a in 0..TARGET_AREA {
-                    let max_y = d - (sensor.y - a).abs();
-                    
-                    if max_y < 0 {
-                        continue;
-                    }
-    
-                    ps.add_sensor(sensor.clone(), d);
-                }
-                
+            if x_range.start < ps.x_min {
+                ps.x_min = x_range.start;
             }
-
-            
-            
-
-            
+            if x_range.end > ps.x_max {
+                ps.x_max = x_range.end;
+            }
+            if y_range.start < ps.y_min {
+                ps.y_min = y_range.start;
+            }
+            if y_range.end > ps.y_max {
+                ps.y_max = y_range.end;
+            }
         }
-
         Ok(ps)
     }
 }
@@ -177,11 +147,11 @@ pub struct Day15{}
 impl Day for Day15 {
     fn run1(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
         let data:PointSet = ipr.whole()?;
-        // data.print();
         Ok(data.count_y_line(TARGET_LINE).to_string())
     }
     
     fn run2(&self, ipr: input_reader::InputReader) -> Result<String, Box<dyn Error>> {
-        Ok(ipr.fullname()?)
+        let data:PointSet = ipr.whole()?;
+        Ok(data.empty_spot(TARGET_AREA).to_string())
     }
 }
